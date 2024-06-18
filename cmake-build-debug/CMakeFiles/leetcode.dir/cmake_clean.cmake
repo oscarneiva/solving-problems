ee47c1ead0a49eb4fd67115cbbd731765e0050c2
oscarneiva <oscarneivaeneto@gmail.com>
2024-06-18 10:46:10 -0300

    obi jogo

---
+++ cmake-build-debug/CMakeFiles/leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode.dir/add-two-numbers.cpp.o"
+  "CMakeFiles/leetcode.dir/add-two-numbers.cpp.o.d"
+  "leetcode"
+  "leetcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
